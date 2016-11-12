0e7450bcb842d9283a0e987acc5977ff4dc92589
Frank Zhang <zhangh94@gmail.com>
2016-11-11 20:12:47 -0800

    Modified ReceivePacket and TransmitRequest to use HexString and ByteSum functions

--- Makefile
+++ Makefile
@@ -8,17 +8,17 @@ CXX_FLAGS=-std=c++11 -Wall -Werror -g
 
 all: Libxbee Test1
 
-Libxbee: Utility TransmitRequest RecievePacket
-	$(CXX) -shared -o $(BUILD_DIR)/libxbee_plus.so $(BUILD_DIR)/TransmitRequest.o $(BUILD_DIR)/RecievePacket.o $(BUILD_DIR)/Utility.o
+Libxbee: Utility TransmitRequest ReceivePacket
+	$(CXX) -shared -o $(BUILD_DIR)/libxbee_plus.so $(BUILD_DIR)/TransmitRequest.o $(BUILD_DIR)/ReceivePacket.o $(BUILD_DIR)/Utility.o
 
 TransmitRequest:
 	$(CXX) $(CXX_FLAGS) -fPIC -c $(LIB_DIR)/TransmitRequest.cpp -o $(BUILD_DIR)/TransmitRequest.o 
 
-RecievePacket:
-	$(CXX) $(CXX_FLAGS) -fPIC -c $(LIB_DIR)/RecievePacket.cpp -o $(BUILD_DIR)/RecievePacket.o 
+ReceivePacket:
+	$(CXX) $(CXX_FLAGS) -fPIC -c $(LIB_DIR)/ReceivePacket.cpp -o $(BUILD_DIR)/ReceivePacket.o
 
 Utility:
-	$(CXX) $(CXX_FLAGS) -fPIC -c $(LIB_DIR)/Utility.cpp -o $(BUILD_DIR)/Utility.o -l$(LIB_DIR)
+	$(CXX) $(CXX_FLAGS) -fPIC -c $(LIB_DIR)/Utility.cpp -o $(BUILD_DIR)/Utility.o
 
 Test1: 
 	$(CXX) $(CXX_FLAGS) $(TEST_DIR)/main.cpp -o $(BUILD_DIR)/Test1 -lboost_system -lboost_thread -L$(BUILD_DIR) -lxbee_plus
--- lib/TransmitRequest.cpp
+++ lib/TransmitRequest.cpp
@@ -5,99 +5,124 @@
 
 #include "../include/TransmitRequest.hpp"
 #include "../include/Xbee.hpp"
+#include "../include/Utility.hpp"
+#include "../lib/Utility.cpp"
 
 namespace XBEE {
-	TransmitRequest::TransmitRequest() {
-		frame_type = 0x10;
-		target_mac_64 = static_cast<uint64_t>(BroadcastType_64::RESERVE_COORDINATOR);
-		target_mac_16 = static_cast<uint16_t>(BroadcastType_16::DEFAULT);
-		TransmitRequest::SetLength();
-		TransmitRequest::SetChecksum();
-	}
 
-	TransmitRequest::TransmitRequest(uint64_t target_64) {
-		frame_type = 0x10;
-		target_mac_64 = target_64;
-		target_mac_16 = static_cast<uint64_t>(BroadcastType_16::DEFAULT);
-		TransmitRequest::SetLength();
-		TransmitRequest::SetChecksum();
-	}
+  TransmitRequest::TransmitRequest() {
+    frame_type = 0x10;
+    target_mac_64 = static_cast<uint64_t> (BroadcastType_64::RESERVE_COORDINATOR);
+    target_mac_16 = static_cast<uint16_t> (BroadcastType_16::DEFAULT);
+    TransmitRequest::SetLength();
+    TransmitRequest::SetChecksum();
+  }
 
-	TransmitRequest::TransmitRequest(uint64_t target_64, uint16_t target_16) {
-		frame_type = 0x10;
-		target_mac_64 = target_64;
-		target_mac_16 = target_16;
-		TransmitRequest::SetLength();
-		TransmitRequest::SetChecksum();
-	}
+  TransmitRequest::TransmitRequest(uint64_t target_64) {
+    frame_type = 0x10;
+    target_mac_64 = target_64;
+    target_mac_16 = static_cast<uint64_t> (BroadcastType_16::DEFAULT);
+    TransmitRequest::SetLength();
+    TransmitRequest::SetChecksum();
+  }
 
-	std::string TransmitRequest::HexString(bool equal_space) const {
-		std::stringstream temp;
-		// Equal Spacing is not yet implemented
-		temp << std::hex << std::uppercase << std::setfill('0');
-		temp << std::setw(2) << static_cast<int>(start);
-		temp << " " << std::setw(4) << static_cast<int>(length);
-		temp << " " << std::setw(2) << static_cast<int>(frame_type);
-		temp << " " << std::setw(2) << static_cast<int>(frame_id);
-		temp << " " << std::setw(16) << target_mac_64;
-		temp << " " << std::setw(4) << target_mac_16;
-		temp << " " << std::setw(2) << static_cast<int>(broadcast_radius);
-		temp << " " << std::setw(2) << static_cast<int>(options);
-		// Bug: Still prints out leading 00 hex characters
-		for(auto itr = data.begin(); itr != data.end(); ++itr) {
-			if(*itr != 0x00)
-				temp << std::setw(2) << static_cast<int>(*itr);
-		}
-		temp << " " << std::setw(2) << static_cast<int>(checksum);
-		return temp.str();
-	}
+  TransmitRequest::TransmitRequest(uint64_t target_64, uint16_t target_16) {
+    frame_type = 0x10;
+    target_mac_64 = target_64;
+    target_mac_16 = target_16;
+    TransmitRequest::SetLength();
+    TransmitRequest::SetChecksum();
+  }
 
-	void TransmitRequest::SetData(const std::string &message) {
-		auto data_itr = data.begin();
-		for(auto letter = message.begin(); letter != message.end(); ++data_itr, ++letter) {
-			*data_itr = *letter;
-		}
-		TransmitRequest::SetLength();
-		TransmitRequest::SetChecksum();
-	}
+  std::string TransmitRequest::ToHexString(Hex_string_format data_format) const {
 
-	void TransmitRequest::SetLength() {
-		length = sizeof(frame_type)
-							+ sizeof(frame_id)
-							+ sizeof(target_mac_64)
-							+ sizeof(target_mac_16)
-							+ sizeof(broadcast_radius)
-							+ sizeof(options);
-		for(auto it = data.begin(); it != data.end(); ++it) {
-			length += (*it != 0x00) ? sizeof(*it) : 0x00;
-		}
-	}
+    std::stringstream tmp;
+    bool is_spaced;
+    bool even_spacing = data_format != DATA_SPACE ? true : false;
 
-	void TransmitRequest::SetChecksum() {
-		uint8_t byte_sum = frame_type + frame_id;
-		byte_sum += ((0xFF00000000000000 & target_mac_64) >> 56);
-		byte_sum += ((0x00FF000000000000 & target_mac_64) >> 48);
-		byte_sum += ((0x0000FF0000000000 & target_mac_64) >> 40);
-		byte_sum += ((0x000000FF00000000 & target_mac_64) >> 32);
-		byte_sum += ((0x00000000FF000000 & target_mac_64) >> 24);
-		byte_sum += ((0x0000000000FF0000 & target_mac_64) >> 16);
-		byte_sum += ((0x000000000000FF00 & target_mac_64) >> 8);
-		byte_sum += (0x00000000000000FF & target_mac_64);
-		byte_sum += ((0xFF00 & target_mac_16) >> 8);
-		byte_sum += (0x00FF & target_mac_16);
-		byte_sum += broadcast_radius;
-		byte_sum += options;
-		for(auto itr = data.begin(); itr != data.end(); ++itr) {
-			byte_sum += *itr;
-		}
-		checksum = kMaxChecksum - byte_sum;
-	}
+    switch (even_spacing) {
+      case true: // even spacisng
+        is_spaced = data_format == BYTE_SPACE ? true : false;
+        tmp << HexString(start, is_spaced, is_spaced);
+        tmp << HexString(length, is_spaced, is_spaced);
+        tmp << HexString(frame_type, is_spaced, is_spaced);
+        tmp << HexString(frame_id, is_spaced, is_spaced);
+        tmp << HexString(target_mac_64, is_spaced, is_spaced);
+        tmp << HexString(target_mac_16, is_spaced, is_spaced);
+        tmp << HexString(broadcast_radius, is_spaced, is_spaced);
+        tmp << HexString(options, is_spaced, is_spaced);
 
-	// Add delimiter support
-	std::istream& operator>>(std::istream &strm, TransmitRequest &tr) {
-		std::string temp;
-		std::getline(strm, temp);
-		tr.SetData(temp);
-		return strm;
-	}
+        for (auto itr = data.begin(); itr != data.end(); ++itr) if (*itr != 0x00) tmp << HexString(*itr, is_spaced, is_spaced);
+        tmp << HexString(checksum, is_spaced);
+        break;
+
+      case false: // not even spacing
+        tmp << HexString(start, false, true);
+        tmp << HexString(length, false, true);
+        tmp << HexString(frame_type, false, true);
+        tmp << HexString(frame_id, false, true);
+        tmp << HexString(target_mac_64, false, true);
+        tmp << HexString(target_mac_16, false, true);
+        tmp << HexString(broadcast_radius, false, true);
+
+        // TODO: Does the bug still exist?
+        tmp << HexString(options, false, true);
+
+        // TODO: Should there be a space between each data member?
+        for (auto itr = data.begin(); itr != data.end(); ++itr) if (*itr != 0x00) tmp << HexString(*itr, false, false);
+        tmp << ' ' << HexString(checksum, false);
+        break;
+
+      default:
+        // TODO: Will this do what I think it does?
+        static_assert(true, "Invalid data_format parameter to TransmitRequest HexString method");
+        break;
+    }
+        return tmp.str();
+  }
+
+  void TransmitRequest::SetData(const std::string &message) {
+    auto data_itr = data.begin();
+    for (auto letter = message.begin(); letter != message.end(); ++data_itr, ++letter) {
+      *data_itr = *letter;
+    }
+    TransmitRequest::SetLength();
+    TransmitRequest::SetChecksum();
+  }
+
+  void TransmitRequest::SetLength() {
+    length = sizeof (frame_type)
+            + sizeof (frame_id)
+            + sizeof (target_mac_64)
+            + sizeof (target_mac_16)
+            + sizeof (broadcast_radius)
+            + sizeof (options);
+    for (auto it = data.begin(); it != data.end(); ++it) {
+      length += (*it != 0x00) ? sizeof (*it) : 0x00;
+    }
+  }
+
+  void TransmitRequest::SetChecksum() {
+
+    uint8_t byte_sum = ByteSum(frame_type);
+    byte_sum += ByteSum(frame_id);
+    byte_sum += ByteSum(target_mac_64);
+    byte_sum += ByteSum(target_mac_16);
+    byte_sum += broadcast_radius;
+    byte_sum += options;
+
+    for (auto itr = data.begin(); itr != data.end(); ++itr) {
+      byte_sum += *itr;
+    }
+
+    checksum = kMaxChecksum - byte_sum;
+  }
+
+  // Add delimiter support
+  std::istream& operator>>(std::istream &strm, TransmitRequest &tr) {
+    std::string temp;
+    std::getline(strm, temp);
+    tr.SetData(temp);
+    return strm;
+  }
 }
